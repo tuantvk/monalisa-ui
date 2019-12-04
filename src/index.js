72074155c33c66a6264ed57d5a44955271dd60e0
TuanTV <tuanhanhphuc123@gmail.com>
2019-12-04 10:00:31 +0700

    update docs publish

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import { ArticleCard } from './components/ArticleCard';
+import {
+  Avatar,
+  AvatarGroup
+} from './components/Avatar';
+import { Badge } from './components/Badge';
+import {
+  Button,
+  ButtonGroup
+} from './components/Button';
+import { Card } from './components/Card';
+import { Col } from './components/Col';
+import { Divider } from './components/Divider';
+import { Icon } from './components/Icon';
+import { Input } from './components/Input';
+import { ListItem } from './components/ListItem';
+import { Pricing } from './components/Pricing';
+import { Rating } from './components/Rating';
+import { Row } from './components/Row';
+import { Segment } from './components/Segment';
+import { SocialIcon } from './components/SocialIcon';
+import { Spinner } from './components/Spinner';
+import { Switch } from './components/Switch';
+import { Tag } from './components/Tag';
+import { Text } from './components/Text';
+import { Textarea } from './components/Textarea';
+
+export {
+  ArticleCard,
+  Avatar,
+  AvatarGroup,
+  Badge,
+  Button,
+  ButtonGroup,
+  Card,
+  Col,
+  Divider,
+  Icon,
+  Input,
+  ListItem,
+  Pricing,
+  Rating,
+  Row,
+  Segment,
+  SocialIcon,
+  Spinner,
+  Switch,
+  Tag,
+  Text,
+  Textarea,
+}
